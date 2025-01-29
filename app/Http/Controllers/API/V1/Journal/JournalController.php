9cffe4b69dfe4e4d44fa8f1e788e1aa1f8146aa1
mdiktushar <md.tushar1804@gmail.com>
2025-01-29 17:34:13 +0600

    journal created

--- app/Http/Controllers/API/V1/Journal/JournalController.php
+++ app/Http/Controllers/API/V1/Journal/JournalController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\API\V1\Journal;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V1\Journal\CreateJournalRequest;
+use App\Services\API\V1\Journal\JournalService;
+use App\Traits\V1\ApiResponse;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class JournalController extends Controller
 {
-    //
+    use ApiResponse;
+    private JournalService $journalService;
+
+    public function __construct(JournalService $journalService)
+    {
+        $this->journalService = $journalService;
+    }
+
+    public function store(CreateJournalRequest $createJournalRequest)
+    {
+        try {
+            $validatedData = $createJournalRequest->validated();
+            $this->journalService->CreateJournal($validatedData);
+        } catch (Exception $e) {
+            Log::error('JournalController::store', [$e->getMessage()]);
+            return $this->error(500, 'Server Error', $e->getMessage());
+        }
+    }
 }
--- app/Services/API/V1/Journal/JournalService.php
+++ app/Services/API/V1/Journal/JournalService.php
@@ -1,8 +1,21 @@
 <?php
-    
+
 namespace App\Services\API\V1\Journal;
+
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class JournalService
 {
-    // Your service logic goes here
+
+
+    public function CreateJournal(array $credentials)
+    {
+        try {
+            
+        } catch (Exception $e) {
+            Log::error('JournalService::CreateJournal', [$e->getMessage()]);
+            throw $e;
+        }
+    }
 }
